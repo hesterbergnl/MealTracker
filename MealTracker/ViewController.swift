ab60bfffba888f3f850ec9b4cc83677cf7251cc4
Nikolai Hesterberg <nlhester@ncsu.edu>
2019-12-14 12:22:14 -0500

    Initial Commit

---
+++ MealTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MealTracker
+//
+//  Created by Nikolai Hesterberg on 12/14/19.
+//  Copyright © 2019 Nikolai Hesterberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
