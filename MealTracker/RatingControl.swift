35a41d3c7e7af595b4842cfc1049aff546a908f1
Nikolai Hesterberg <nlhester@ncsu.edu>
2019-12-16 22:28:48 -0500

    Added initial implementation of the star-rating custom control

---
+++ MealTracker/RatingControl.swift
@@ -0,0 +1,19 @@
+//  RatingControl.swift
+//  MealTracker
+//
+//  Created by Nikolai Hesterberg on 12/16/19.
+//  Copyright © 2019 Nikolai Hesterberg. All rights reserved.
+
+import UIKit
+
+class RatingControl: UIStackView {
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
