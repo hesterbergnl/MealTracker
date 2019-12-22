53e4e32e011ad64a6554853e56559936f2f3eb38
Nikolai Hesterberg <nlhester@ncsu.edu>
2019-12-21 23:44:54 -0500

    Added a new scene (list view) and started implementing model code for it

---
+++ MealTracker/MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealTableViewCell.swift
+//  MealTracker
+//
+//  Created by Nikolai Hesterberg on 12/21/19.
+//  Copyright © 2019 Nikolai Hesterberg. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
